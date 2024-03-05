45bc0da830ac14cbd13a1531dddba03db43ee702
RobertoGuzman64 <robertoguzman64@gmail.com>
2024-03-05 12:09:33 +0100

    seccion 3

--- nextjs/src/app/api/notes/[id]/route.ts
+++ nextjs/src/app/api/notes/[id]/route.ts
@@ -1,20 +1,99 @@
 import { NextResponse } from "next/server";
+import { prisma } from "@/libs/prisma";
 
-export function GET(){
-    return NextResponse.json({
-        message: "Obteniendo una Nota"
-    })
+interface Params { params: { id: string } }
+
+export async function GET(request: Request, { params }: Params) {
+    try {
+        const note = await prisma.note.findFirst({
+            where: {
+                id: Number(params.id)
+            }
+        })
+        if (!note) return NextResponse.json(
+            {
+                message: "No existe la Nota"
+            },
+            {
+                status: 404
+            }
+        )
+        return NextResponse.json(note);
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json(
+                {
+                    message: error.message,
+                },
+                {
+                    status: 500
+                }
+            );
+        }
+    }
 }
 
-export function PUT(){
-    return NextResponse.json({
-        message: "Actualizando una Nota"
-    })
+export async function PUT(request: Request, { params }: Params) {
+    try {
+        const { title, content } = await request.json();
+        const updatedNote = await prisma.note.update({
+            where: {
+                id: Number(params.id)
+            },
+            data: {
+                title,
+                content
+            },
+        });
+        if (!updatedNote) return NextResponse.json(
+            {
+                message: "No existe la Nota"
+            },
+            {
+                status: 404
+            }
+        )
+        return NextResponse.json(updatedNote);
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json(
+                {
+                    message: error.message,
+                },
+                {
+                    status: 500
+                }
+            );
+        }
+    }
 }
 
-export function DELETE(){
-    return NextResponse.json({
-        message: "Borrando una Nota"
-    })
+export async function DELETE(request: Request, { params }: Params) {
+    try {
+        const deletedNote = await prisma.note.delete({
+            where: {
+                id: Number(params.id)
+            }
+        })
+        if (!deletedNote) return NextResponse.json(
+            {
+                message: "No existe la Nota"
+            },
+            {
+                status: 404
+            }
+        )
+        return NextResponse.json(deletedNote);
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json(
+                {
+                    message: error.message,
+                },
+                {
+                    status: 500
+                }
+            );
+        }
+    }
 }
-
--- nextjs/src/app/api/notes/route.ts
+++ nextjs/src/app/api/notes/route.ts
@@ -1,12 +1,45 @@
 import { NextResponse } from "next/server";
 
-export function GET(){
-    return NextResponse.json({
-        message: 'Viendo las Notas',
-    })
+import { prisma } from "@/libs/prisma"
+
+export async function GET() {
+    try {
+        const notes = await prisma.note.findMany()
+        return NextResponse.json(notes)
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json(
+                {
+                    message: error.message,
+                },
+                {
+                    status: 500
+                }
+            );
+        }
+    }
 }
-export function POST(){
-    return NextResponse.json({
-        message: 'Creando las Notas',
-    })
+
+export async function POST(request: Request) {
+    try{
+        const { title, content } = await request.json()
+        const newNote = await prisma.note.create({
+            data: {
+                title,
+                content
+            }
+        });
+        return NextResponse.json(newNote)
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json(
+                {
+                    message: error.message,
+                },
+                {
+                    status: 500
+                }
+            );
+        }
+    }
 }
