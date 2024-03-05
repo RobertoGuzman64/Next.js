66074b8d256409a0a73b3a892f6d79d5ea520516
RobertoGuzman64 <robertoguzman64@gmail.com>
2024-03-05 13:36:43 +0100

    seccion 4

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,7 +1,32 @@
-export default function HomePage() {
+import NoteForm from "@/components/NoteForm"
+
+interface Note {
+  id: number;
+  title: string;
+  content: string;
+}
+
+async function loadNotes() {
+  const res = await fetch("http://localhost:3000/api/notes");
+  const data = await res.json();
+  return data
+}
+
+export default async function HomePage() {
+  const notes = await loadNotes();
+  console.log({ notes });
   return (
-    <div>
-      <h1>Página Principal</h1>
+    <div className="flex items-center justify-center h-screen">
+      <div>
+        <h1>Página Principal</h1>
+        <NoteForm />
+        {notes.map((note: Note) => (
+          <div key={note.id} className="bg-slate-400 p-4 my-2">
+            <h1>{note.title}</h1>
+            <p>{note.content}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
