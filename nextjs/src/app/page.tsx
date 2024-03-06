d3e2e6b313408f41eac9b5e593068b8a5e6039d7
RobertoGuzman64 <robertoguzman64@gmail.com>
2024-03-06 10:22:59 +0100

    seccion 6

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,32 +1,28 @@
-import NoteForm from "@/components/NoteForm"
+"use client";
 
-interface Note {
-  id: number;
-  title: string;
-  content: string;
-}
+import NoteForm from "@/components/NoteForm";
+import NoteCard from "@/components/NoteCard";
+import { useNotes } from "@/context/NoteContext";
+import { useEffect } from "react";
 
-async function loadNotes() {
-  const res = await fetch("http://localhost:3000/api/notes");
-  const data = await res.json();
-  return data
-}
+function HomePage() {
+  const { notes, loadNotes } = useNotes();
 
-export default async function HomePage() {
-  const notes = await loadNotes();
-  console.log({ notes });
+  useEffect(() => {
+    loadNotes();
+  });
+
   return (
     <div className="flex items-center justify-center h-screen">
       <div>
-        <h1>Página Principal</h1>
         <NoteForm />
-        {notes.map((note: Note) => (
-          <div key={note.id} className="bg-slate-400 p-4 my-2">
-            <h1>{note.title}</h1>
-            <p>{note.content}</p>
-          </div>
+
+        {notes.map((note) => (
+          <NoteCard note={note} key={note.id} />
         ))}
       </div>
     </div>
   );
 }
+
+export default HomePage;
--- nextjs/src/components/NoteForm.tsx
+++ nextjs/src/components/NoteForm.tsx
@@ -1,42 +1,89 @@
 "use client";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
+import { useState, useRef, useEffect } from "react";
+import { useNotes } from "@/context/NoteContext";
 
-export default function NoteForm() {
-    const [title, setTitle] = useState('')
-    const [content, setContent] = useState('')
+function NoteForm() {
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const titleRef = useRef<HTMLInputElement>(null);
 
-    const router = useRouter()
+    const { createNote, selectedNote, setSelectedNote, updateNote } = useNotes();
+
+    useEffect(() => {
+        if (selectedNote) {
+            setTitle(selectedNote.title);
+            setContent(selectedNote.content || "");
+        }
+    }, [selectedNote]);
 
     return (
         <form
             onSubmit={async (e) => {
-                e.preventDefault()
-                const res = await fetch('/api/notes', {
-                    method: 'POST',
-                    body: JSON.stringify({ title, content }),
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                })
-                const data = await res.json()
-                console.log(data)
-                router.refresh()
-            }}>
+                e.preventDefault();
+
+                if (selectedNote) {
+                    await updateNote(selectedNote.id, {
+                        title,
+                        content,
+                    })
+                    setSelectedNote(null)
+                } else {
+                    await createNote({
+                        title,
+                        content,
+                    });
+                }
+
+                setTitle("");
+                setContent("");
+
+                titleRef.current?.focus();
+            }}
+        >
             <input
                 type="text"
                 name="title"
-                autoFocus placeholder="Title"
+                autoFocus
+                placeholder="Title"
                 className="w-full px-4 py-2 text-black bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 my-2"
-                onChange={(e) => setTitle(e.target.value)}>
-            </input>
+                onChange={(e) => setTitle(e.target.value)}
+                value={title}
+                ref={titleRef}
+            />
+
             <textarea
                 name="title"
                 placeholder="Content"
                 className="w-full px-4 py-2 text-black bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 my-2"
-                onChange={(e) => setContent(e.target.value)}>
-            </textarea>
-            <button className="px-5 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700">Create</button>
+                onChange={(e) => setContent(e.target.value)}
+                value={content}
+            ></textarea>
+
+            <div className="flex justify-end gap-x-2">
+                <button
+                    className="px-5 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 disabled:opacity-50 disabled:cursor-not-allowed"
+                    disabled={!title || !content}
+                    type="submit"
+                >
+                    {selectedNote ? "Update" : "Create"}
+                </button>
+
+                {selectedNote && (
+                    <button
+                        className="px-5 py-2 text-black bg-slate-400 hover:bg-slate-500 rounded-md"
+                        type="button"
+                        onClick={() => {
+                            setSelectedNote(null);
+                            setTitle("");
+                            setContent("");
+                        }}
+                    >
+                        Cancel
+                    </button>
+                )}
+            </div>
         </form>
-    )
+    );
 }
+
+export default NoteForm;
